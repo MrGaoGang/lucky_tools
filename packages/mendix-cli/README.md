e6acb11436037009cd771a2d429b75c6dc18e535
mr.gao <gaogangwork@qq.com>
2019-09-28 10:11:23 +0800

    feat: init tools project

---
+++ packages/mendix-cli/README.md
@@ -0,0 +1,11 @@
+# `mendix-cli`
+
+> TODO: description
+
+## Usage
+
+```
+const mendixCli = require('mendix-cli');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/mendix-cli/bin/index.js
@@ -0,0 +1,10 @@
+let minimist = require("minimist");
+let program = require("commander");
+program.version(require("../package.json").version).usage('<command> [options]');
+program.command("create <app-name>")
+    .description("create a new project powered by mendix-widgets-cli")
+    .action(function (name, cmd) {
+        console.log('====================================');
+        console.log(name, cmd);
+        console.log('====================================');
+    })
